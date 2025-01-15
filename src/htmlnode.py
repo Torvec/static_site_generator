902bf75f06473dd09af74af5440a1ebe0e02afd7
vonedward <me@edward-vonschondorf.dev>
2025-01-14 21:49:42 -0800

    Add leafnode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,11 +12,23 @@ class HTMLNode:
         if isinstance(self.props, dict):
             props_list = []
             for key, value in self.props.items():
-                prop_str = f'{key}="{value}"'
+                prop_str = f' {key}="{value}"'
                 props_list.append(prop_str)
-            return ' '.join(props_list)
+            return ''.join(props_list)
         return ""
     
     def __repr__(self):
         return f"HTMLNode (Tag: {self.tag} Value: {self.value} Children: {self.children} Props: {self.props})"
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value == None or not self.value:
+            raise ValueError
+        if self.tag == None:
+            return f"{self.value}"
+        if self.props == None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,35 +1,60 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_is_empty(self):
+    def test_HTMLNode_is_empty(self):
         node = HTMLNode()
         self.assertIsNone(node.tag)
         self.assertIsNone(node.value)
         self.assertIsNone(node.children)
         self.assertIsNone(node.props)
     
-    def test_is_not_empty(self):
+    def test_HTMLNode_is_not_empty(self):
         node = HTMLNode("p")
         self.assertIsNotNone(node)
     
-    def test_props_to_html(self):
+    def test_HTMLNode_props_to_html(self):
         props = {
             "href": "https://www.google.com",
             "target": "_blank",
             "id": "testing"
         }
         node = HTMLNode(props=props)
-        self.assertEqual(node.props_to_html(), 'href="https://www.google.com" target="_blank" id="testing"')
+        self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank" id="testing"')
     
-    def test_repr(self):
+    def test_HTMLNode_repr(self):
         tag = "p"
         value = "Some Value"
         children = []
         props = {"id":"test"}
         node = HTMLNode(tag=tag, value=value, children=children, props=props)
         self.assertEqual(node.__repr__(), f"HTMLNode (Tag: {tag} Value: {value} Children: {children} Props: {props})")
+
+class TestLeafNode(unittest.TestCase):
+    def test_LeafNode_value_err(self):
+        node = LeafNode(tag=None, value=None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+    
+    def test_LeafNode_text_output(self):
+        tag=None
+        value = "Some Text For Ya"
+        node = LeafNode(tag=tag, value=value)
+        self.assertEqual(node.to_html(), f"{value}")
+
+    def test_LeafNode_tag_no_attr_output(self):
+        tag = "p"
+        value = "Some values yo"
+        node = LeafNode(tag=tag, value=value)
+        self.assertEqual(node.to_html(), f"<{tag}>{value}</{tag}>")
+
+    def test_LeafNode_tag_output(self):
+        tag = "p"
+        value = "Text In a Tag"
+        props = {"class":"text"}
+        node = LeafNode(tag=tag, value=value, props=props)
+        self.assertEqual(node.to_html(), f"<{tag} {node.props_to_html()}>{value}</{tag}>")
 
 if __name__ == "__main__":
     unittest.main()
