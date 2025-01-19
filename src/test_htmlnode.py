669a07f2cdeaa9b622fbaf4fe1ae2a8a14638404
vonedward <me@edward-vonschondorf.dev>
2025-01-18 16:38:36 -0800

    Add leafnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_empty(self):
@@ -20,6 +20,32 @@ class TestHTMLNode(unittest.TestCase):
         with self.assertRaises(NotImplementedError) as context:
             node.to_html()
         self.assertEqual(str(context.exception), "Method not implemented")
+
+class TestLeafNode(unittest.TestCase):
+    def test_value_err(self):
+        node = LeafNode("p", None, None)
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertEqual(str(context.exception), "A value is required")
+
+    def test_text_output(self):
+        node = LeafNode(None, "Raw Text Here", None)
+        self.assertEqual(node.to_html(), "Raw Text Here")
+
+    def test_html_output(self):
+        node = LeafNode("p", "This is a paragraph", None)
+        self.assertEqual(node.to_html(), "<p>This is a paragraph</p>")
+
+    def test_html_output_with_props(self):
+        node = LeafNode("p", "This is another paragraph", {"id":"paragraph", "class":"intro"})
+        self.assertEqual(node.to_html(), '<p id="paragraph" class="intro">This is another paragraph</p>')
+
+    def test_html_output_for_link(self):
+        node = LeafNode("a", "A link", {"href":"https://www.boot.dev/", "target":"_blank"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev/" target="_blank">A link</a>')
+
+    def test_html_output_with_children(self):
+        node = LeafNode("p", "Some Text", )
 
 if __name__ == "__main__":
     unittest.main()
